ef0a83eafeefbdbeebc7c70b08eb2e96c5f308b0
khairulislam9964 <khairulislam9964@gmail.com>
2022-07-29 21:39:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const profile = document.querySelector(".profile img");
+const credit = document.querySelector(".credit");
+const description = document.querySelector(".info h3");
+const socials = document.querySelector(".socials");
+//Moving Animation Event
+container.addEventListener("mousemove",(e) => { 
+	let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+	let yAxis = (window.innerHeight / 2 - e.pageY) /25;
+	card.style.transform =`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => { 
+	card.style.transition= "none";
+	//Popout
+	title.style.transform = "translateZ(150px)";
+	profile.style.transform = "translateZ(200px)rotateZ(-45deg)";
+	description.style.transform = "translateZ(125px)";
+	socials.style.transform = "translateZ(100px)";
+	credit.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container.addEventListener("mouseleave",(e) => {
+	card.style.transition = "all 0.5s ease";
+	card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+	//Popback
+	title.style.transform = "translateZ(0px)";
+	profile.style.transform = "translateZ(0px)rotateZ(0deg)";
+	description.style.transform = "translateZ(0px)";
+	socials.style.transform = "translateZ(0px)";
+	credit.style.transform = "translateZ(0px)";
+});
+	
